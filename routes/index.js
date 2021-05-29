fd309b25be1277e6b08b51fb1430730326b9732a
jjjjonathan <hornathanjon@gmail.com>
2021-05-29 16:32:39 -0500

    application complete

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var formatDistanceToNow = require('date-fns/formatDistanceToNow');
 
 const messages = [
   {
     text: 'Hi there!',
     user: 'Amando',
-    added: new Date(),
+    added: new Date('02/02/2021'),
   },
   {
     text: 'Hello World!',
@@ -16,7 +17,28 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Message Board', messages });
+  res.render('index', {
+    title: 'Mini Message Board',
+    messages: messages.map((message) => {
+      return {
+        ...message,
+        added: formatDistanceToNow(message.added, {
+          addSuffix: true,
+          includeSeconds: true,
+        }),
+      };
+    }),
+  });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  const { message, name } = req.body;
+  messages.push({ text: message, user: name, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
